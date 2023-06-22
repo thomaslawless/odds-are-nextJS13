6f83bc507b236f04870dcb0cd0abadab8a4851ba
Thomas Lawless <lawlessptom@gmail.com>
2023-06-22 18:00:10 -0400

    Tables listing for bets - for now

--- app/groups/[id]/page.tsx
+++ app/groups/[id]/page.tsx
@@ -1,23 +1,88 @@
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
+
+const bets = [
+  {
+    bet: "This is Bet Number One",
+    status: "Pending",
+    betAmount: "$250.00",
+    odds: "+200",
+    User: "@Tom",
+  },
+  {
+    bet: "This is Bet Number One",
+    status: "Pending",
+    betAmount: "$250.00",
+    odds: "+200",
+    User: "@Tom",
+  },
+  {
+    bet: "This is Bet Number One",
+    status: "Pending",
+    betAmount: "$250.00",
+    odds: "+200",
+    User: "@Tom",
+  },
+  {
+    bet: "This is Bet Number One",
+    status: "Pending",
+    betAmount: "$250.00",
+    odds: "+200",
+    User: "@Tom",
+  },
+  {
+    bet: "This is Bet Number One",
+    status: "Pending",
+    betAmount: "$250.00",
+    odds: "+200",
+    User: "@Tom",
+  },
+  {
+    bet: "INV001",
+    status: "Paid",
+    betAmount: "$250.00",
+    odds: "+200",
+    User: "@Tom",
+  },
+  {
+    bet: "INV001",
+    status: "Paid",
+    betAmount: "$250.00",
+    odds: "+200",
+    user: "@Tom",
+  },
+]
+
 export default function Page() {
   return (
-    <div>
-      <table>
-  <tr>
-    <th>Bet</th>
-    <th>Odds</th>
-    <th>User</th>
-  </tr>
-  <tr>
-    <td>Bet 1</td>
-    <td>+100</td>
-    <td>@tom</td>
-  </tr>
-  <tr>
-    <td>Bet 2</td>
-    <td>+200</td>
-    <td>@john</td>
-  </tr>
-</table>
-    </div>
+    <Table>
+      <TableCaption>Bets for Group Name</TableCaption>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[100px]">Bet</TableHead>
+          <TableHead>Status</TableHead>
+          <TableHead>Amount</TableHead>
+          <TableHead>Odds</TableHead>
+          <TableHead className="text-right">User</TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {bets.map((bets) => (
+          <TableRow key={bets.bet}>
+            <TableCell>{bets.status}</TableCell>
+            <TableCell>{bets.betAmount}</TableCell>
+            <TableCell>{bets.odds}</TableCell>
+            <TableCell>{bets.user}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
   )
 }
